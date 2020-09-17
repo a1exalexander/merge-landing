65bced07f431e316015296b98aa01aef14938aab
Alex Ratushnyi <alexander.ratushnyi@gmail.com>
2020-09-17 19:50:08 +0300

    ✨ Add Dribble Slider

--- src/js/main.js
+++ src/js/main.js
@@ -65,18 +65,53 @@ const installHeaderStyles = () => {
   const $header = $('.header');
   const $bookButtonMobile = $('#header-book-button-mobile');
 
-  $(document).on('scroll', (e, d) => {
-    const offset = $(document).scrollTop();
-    if (offset > 40 && !$header.hasClass('_shadow')) {
-      $header.addClass('_shadow');
-      $bookButtonMobile.removeClass('button--black');
-    } else if (offset <= 40 && $header.hasClass('_shadow')) {
-      $header.removeClass('_shadow');
-      $bookButtonMobile.addClass('button--black');
-    }
-  });
+  if ($header.length && $bookButtonMobile.length) {
+    $(document).on('scroll', (e, d) => {
+      const offset = $(document).scrollTop();
+      if (offset > 40 && !$header.hasClass('_shadow')) {
+        $header.addClass('_shadow');
+        $bookButtonMobile.removeClass('button--black');
+      } else if (offset <= 40 && $header.hasClass('_shadow')) {
+        $header.removeClass('_shadow');
+        $bookButtonMobile.addClass('button--black');
+      }
+    });
+  }
+};
+
+const installDribbleSlider = () => {
+  const delay = 5000;
+  const $slider = $('#dribble-slider');
+  const $slides = $slider.find('img');
+  let slideIdx = 0;
+  let slidesLength = 0;
+  if (!$slider.hasClass('_active') && $slider.length && $slides.length > 1) {
+    $slider.addClass('_active');
+    slidesLength = $slides.length - 1;
+    $slides.each((idx, el) => {
+      if (idx === 0) {
+        $(el).addClass('uk-animation-kenburns _active');
+      } else {
+        $(el).hide();
+      }
+    });
+    let timer = setTimeout(function slider() {
+      const oldIdx = slideIdx;
+      if (slideIdx < slidesLength) {
+        slideIdx += 1;
+      } else {
+        slideIdx = 0;
+      }
+
+      $slides.eq(slideIdx).addClass('uk-animation-kenburns _active').fadeIn(1000);
+      $.when($slides.eq(oldIdx).removeClass('_active').fadeOut(1000)).then(() => {
+        $slides.eq(oldIdx).removeClass('uk-animation-kenburns');
+      })
+      timer = setTimeout(slider, delay);
+    }, delay);
+  }
 };
 
 $(() => {
-  fuse(installModals, installComments, installAOS, installHeaderStyles);
+  fuse(installModals, installComments, installAOS, installHeaderStyles, installDribbleSlider);
 });
