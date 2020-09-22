edc15f67af88b84514213cf4f538d302eeba9dc8
Alex Ratushnyi <alexander.ratushnyi@gmail.com>
2020-09-22 19:43:32 +0300

    🐛Fix webpack config

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,6 +1,7 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const helpers = require('./webpack.helpers');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = (env) => {
   return merge(common, {
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -2,7 +2,6 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const ImageminPlugin = require('imagemin-webpack');
 const helpers = require('./webpack.helpers');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = (env) => {
   return merge(common, {
